2b79b767dd5903fe34de70c18b53f5dfb77ab023
Girish Raut <142937451+clashingera@users.noreply.github.com>
2025-04-29 08:31:44 +0000

    5 done

--- 5_Min_Max.cpp
+++ 5_Min_Max.cpp
@@ -1,86 +1,68 @@
-EXP 5
+// EXP 5
 #include <iostream>
+//#include <vector>
 #include <omp.h>
 #include <climits>
 using namespace std;
-void min_reduction(int arr[], int n)
-{
-    int min_value = INT_MAX;
-#pragma omp parallel for reduction(min : min_value)
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] < min_value)
-        {
-            min_value = arr[i];
-        }
-    }
-    cout << "Minimum value: " << min_value << endl;
+void min_reduction(int arr[], int n) {
+  int min_value = INT_MAX;
+  #pragma omp parallel for reduction(min: min_value)
+  for (int i = 0; i < n; i++) {
+	if (arr[i] < min_value) {
+  	min_value = arr[i];
+	}
+  }
+  cout << "Minimum value: " << min_value << endl;
 }
-void max_reduction(int arr[], int n)
-{
-    int max_value = INT_MIN;
-#pragma omp parallel for reduction(max : max_value)
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] > max_value)
-        {
-            max_value = arr[i];
-        }
-    }
-    cout << "Maximum value: " << max_value << endl;
-}
-void sum_reduction(int arr[], int n)
-{
-    int sum = 0;
-#pragma omp parallel for reduction(+ : sum)
-    for (int i = 0; i < n; i++)
-    {
-        sum += arr[i];
-    }
-    cout << "Sum: " << sum << endl;
-}
-void average_reduction(int arr[], int n)
-{
-    if (n <= 1)
-    {
-        cout << "Average: Cannot calculate (array size too small)" << endl;
-        return;
-    }
-    int sum = 0;
-#pragma omp parallel for reduction(+ : sum)
-    for (int i = 0; i < n; i++)
-    {
-        sum += arr[i];
-    }
-    cout << "Average: " << static_cast<double>(sum) / n << endl;
-}
-int main()
-{
-    cout << "\n\nName: Girish Raut\nRoll No.39 \t Div.B\n\n";
-    int *arr, n;
-    cout << "\nEnter total number of elements: ";
-    cin >> n;
 
-    if (n <= 0)
+void max_reduction(int arr[], int n) {
+  int max_value = INT_MIN;
+  #pragma omp parallel for reduction(max: max_value)
+  for (int i = 0; i < n; i++) {
+	if (arr[i] > max_value) {
+  	max_value = arr[i];
+	}
+  }
+  cout << "Maximum value: " << max_value << endl;
+}
+
+void sum_reduction(int arr[], int n) {
+  int sum = 0;
+   #pragma omp parallel for reduction(+: sum)
+   for (int i = 0; i < n; i++) {
+	sum += arr[i];
+  }
+  cout << "Sum: " << sum << endl;
+}
+
+void average_reduction(int arr[], int n) {
+  int sum = 0;
+  #pragma omp parallel for reduction(+: sum)
+  for (int i = 0; i < n; i++) {
+	sum += arr[i];
+  }
+  cout << "Average: " << (double)sum / (n-1) << endl;
+}
+
+int main() {
+    int *arr,n;
+    cout<<"\n enter total no of elements=>";
+    cin>>n;
+    arr=new int[n];
+    cout<<"\n enter elements=>";
+    for(int i=0;i<n;i++)
     {
-        cerr << "Error: Array size must be positive" << endl;
-        return 1;
+   	 cin>>arr[i];
     }
 
-    arr = new int[n];
-    cout << "\nEnter elements:\n";
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    min_reduction(arr, n);
-    max_reduction(arr, n);
-    sum_reduction(arr, n);
-    average_reduction(arr, n);
+//   int arr[] = {5, 2, 9, 1, 7, 6, 8, 3, 4};
+//   int n = size(arr);
 
-    delete[] arr;
-    return 0;
+  min_reduction(arr, n);
+  max_reduction(arr, n);
+  sum_reduction(arr, n);
+  average_reduction(arr, n);
 }
 // Run Commands:
 // g++ -fopenmp -o parallel_reduction 5_Min_Max.cpp
-// .\parallel_reduction
+// ./parallel_reduction
